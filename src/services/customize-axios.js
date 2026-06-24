742cfcf92b53cf77b606fa059ba27a3593d63abb
lucidjug <tuuju2@gmail.com>
2026-06-24 22:19:28 +0700

    Update

--- src/services/customize-axios.js
+++ src/services/customize-axios.js
@@ -1,8 +1,8 @@
 import axios from "axios";
 
 const api = axios.create({
-  // baseURL: "https://hotel-booking-api-xojn.onrender.com",
-  baseURL: "http://localhost:8080",
+  baseURL: "https://hotel-booking-api-xojn.onrender.com",
+  // baseURL: "http://localhost:8080",
 });
 
 
